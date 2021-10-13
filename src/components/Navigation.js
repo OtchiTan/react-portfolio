5562b024b3648ec96285469ee4b53abfeef382c3
Adam <adamgragnic@gmail.com>
2021-10-13 19:37:59 +0200

    Importation de react

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import { AppBar, Toolbar } from '@mui/material';
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <AppBar sx={{position:"relative"}}>
+            <Toolbar>
+                
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Navigation;
