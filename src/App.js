b0b7d41e534ec1d60ed4917e030a71c17ada54da
Adam <adamgragnic@gmail.com>
2021-10-21 19:21:01 +0200

    Ajout du component de Projet avec des Cards

--- src/App.js
+++ src/App.js
@@ -3,17 +3,38 @@ import Home from "./pages/Home"
 import NotFound from './pages/NotFound';
 import Projets from './pages/Projets';
 import Competences from './pages/Competences'
+import { createTheme } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#8561c5',
+      main: '#673ab7',
+      dark: '#482880',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#6a52b3',
+      main: '#4527a0',
+      dark: '#301b70',
+      contrastText: '#fff',
+    },
+  },
+})
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home}/>
-        <Route path="/projets" exact component={Projets}/>
-        <Route path="/competences" exact component={Competences}/>
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/projets" exact component={Projets}/>
+          <Route path="/competences" exact component={Competences}/>
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Projets.js
+++ src/pages/Projets.js
@@ -1,37 +1,34 @@
-import { Container, Typography } from '@mui/material';
-import React, { useState } from 'react';
+import { Button, Card, CardActions, CardContent, CardHeader, CardMedia, Container, Typography } from '@mui/material';
 import Navigation from '../components/Navigation';
 
-const axios = require('axios')
-
-
-/*function PorjectContent(props){
+const ProjectContent = (props) => {
     return (
-        <Container sx={{display:'flex'}}>
-            <Box sx={{border:'solid black'}}>
-
-            </Box>
-            <Box sx={{border:'solid black'}}>
-                <img src={props.imgUrl} alt={props.imgAlt} />
-            </Box>
-        </Container>
+        <Card sx={{maxWidth: "80%", marginY:"1rem"}}>
+            <CardHeader title={props.title}/>
+            <CardMedia component="img" image={props.imgUrl}/>
+            <CardContent>
+                <Typography>{props.desc}</Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">More</Button>
+            </CardActions>
+        </Card>
     )
 }
-*/
-const Projets = () => {
-    const [test, setTest] = useState("")
-    
-    axios.get('http://localhost:8000')
-            .then((res) => {
-                setTest(res.data)
-            })
 
+const Projets = () => {
     return (
         <div>
             <Navigation/>
-            <Container maxWidth='md' sx={{ marginY:'2%' }}>
+            <Container maxWidth='md' sx={{ 
+                marginY:'2%',
+                display:"flex",
+                flexDirection:"column",
+                alignItems:"center" }}
+                >
                 <Typography variant="h3">Projets</Typography>
-                {test}
+                <ProjectContent title="Captain Battle" imgUrl="/cb_1.jpg" desc="Desc 1"/>
+                <ProjectContent title="Magic Project" imgUrl="/mp_1.jpg" desc="Desc 2"/>
             </Container>
         </div>
     );
